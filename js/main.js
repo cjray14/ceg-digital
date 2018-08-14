335dfc27804f7549f8a2b4009bbae23b1bfb5d73
Chris Ray <30697743+cjray14@users.noreply.github.com>
2018-08-14 17:59:46 +0100

    Site building

--- js/main.js
+++ js/main.js
@@ -1,13 +1,47 @@
-var express = require('express')
-var app = express()
+function pageLoader(){
+  var bar = new ProgressBar.Line(container, {
+    strokeWidth: 4,
+    easing: 'easeInOut',
+    duration: 1000,
+    color: '#ee7700',
+    trailColor: '#fff',
+    trailWidth: 1,
+    svgStyle: {width: '100%', height: '100%'},
+    from: {color: '#fff'},
+    to: {color: '#ee7700'},
+    step: (state, bar) => {
+      bar.path.setAttribute('stroke', state.color);
+    }
+  });
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+  bar.animate(1.0);
+}
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+function blogCarousel(){
+  if ($('.blg-List_Items .blg-List_Item').length > 1) {
+    $('.blg-List_Items').flickity({
+      cellAlign: 'left',
+      wrapAround: true,
+      imagesLoaded: true,
+      pageDots: false,
+      arrowShape: {
+        x0: 30,
+        x1: 60, y1: 20,
+        x2: 60, y2: 10,
+        x3: 60
+      }
+    });
+  };
+}
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+function globals(){
+  pageLoader()
+  blogCarousel()
+}
+
+$(document).ready(function(){
+  $('.js-LoadDelay').addClass('js-LoadDelay-loaded');
+  $('.js-LoadDelayAfter').addClass('js-LoadDelay-loaded');
+
+  globals()
+});
